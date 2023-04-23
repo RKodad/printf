05e942bb280b5c1b3ca293effc8d92cddd4a50c9
RKodad <rn.kodad@gmail.com>
2023-04-23 13:54:49 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -1,58 +1,52 @@
 #include <stdarg.h>
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stddef.h>
+/**
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	if (format != NULL)
+	{
+		int count = 0, i;
+		int (*m)(va_list);
+		va_list args;
 
-int _printf(const char *format, ...){
-  int count = 0, i;
-  
-  va_list data;
-  va_start(data, format);
-
-  /* _printf("%s", 'Hello') */
-  
-  for (i = 0; format[i] != '\0'; ){
-    
-    /* count the number of characters */
-    /* print to the screen each character counted */
-    if (format[i] != '%'){
-      count += _putchar(format[i]);
-      i++;
-    }
-    else if (format[i] == '%' && format[i+1] !=' '){
-      switch (format[i + 1]){
-        case 'c':
-            /* print the character from the va_arguments */
-            count += _putchar(va_arg(data, int));
-            break;
-        case 's':
-            count += print_string(va_arg(data, char *));
-            break;
-        case '%':
-            /* print the character from the va_arguments */
-            count += _putchar('%');
-            break;
-        case 'd':
-            count += print_decimal(va_arg(data, int));
-            break;
-        case 'i':
-            count += print_decimal(va_arg(data, int));
-            break;
-        case 'b':
-            count += print_binary(va_arg(data, int));
-            break;
-        case 'u':
-            count += print_unsigned(va_arg(data, unsigned int));
-            break;
-        default:
-            break;
-      }
-      
-      i += 2;
-    }
-    
-    
-  }
-  
-
-  return (count);
+		va_start(args, format);
+		i = 0;
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+		while (format != NULL && format[i] != '\0')
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					m = get_func(format[i + 1]);
+					if (m)
+						count += m(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+					i += 2;
+				}
+			}
+			else
+			{
+				count += _putchar(format[i]);
+				i++;
+			}
+		}
+		va_end(args);
+		return (count);
+	}
+	return (-1);
 }
